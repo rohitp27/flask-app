39a89d43eb0c82aa68cd72735b6cd82d051ebf67
Rohit Pandey <bitsemails@gmail.com>
2018-10-23 01:14:43 +0530

    Added templates for demo

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from application import application
 
 @application.route('/')
-
 @application.route('/index')
 def index():
-	return "Hello World!"
+	user = {'username' : 'Rohit Pandey'}
+	posts= [
+		{
+			'author':{'username' : 'Suri'},
+			'body' : 'I say meow!'
+		},
+		{
+			'author':{'username' : 'Noell'},
+			'body' : 'Me too!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
+
